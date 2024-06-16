16a281f2d88a503f0677811b8ddf126e9a0d8b85
Star <101126792+Starx707@users.noreply.github.com>
2024-06-17 00:07:42 +0200

    Tutorial scene complete
    - More added to map
    - Scripts & behaviour up to date

--- Assets/Scripts/DialogNPC.cs
+++ Assets/Scripts/DialogNPC.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _dialogPanel;
+    [SerializeField] private TMP_Text _dialogTxt;
+    [SerializeField] private string _dialog;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        _dialogPanel.SetActive(true);
+        _dialogTxt.text = _dialog;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        _dialogPanel.SetActive(false);
     }
 }
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -8,37 +8,53 @@ using UnityEngine.SceneManagement;
 public class GameState : MonoBehaviour
 {
     [SerializeField] private GameManager _gm;
-    public int coinsGoal = 4;
-    public int killGoal = 6;
+    public int coinsGoal = 3; //Change before build
+    public int killGoal = 3; //Change before build
 
     [SerializeField] private TMP_Text _dialogNPC; //dialog in the UI
     [SerializeField] private Color _colorDialogNPC; //color depending on which of two is talking
+    [SerializeField] private GameObject _dialogObject;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (_gm.collectedCoins >= coinsGoal && _gm.enemiesDefeated >= killGoal)
+            if (_gm.collectedCoins >= coinsGoal && _gm.enemyDeathCount >= killGoal)
             {
                 SceneManager.LoadSceneAsync("GameWon");
             }
-            else if (_gm.collectedCoins >= coinsGoal && _gm.enemiesDefeated >= killGoal)
+            else if (_gm.collectedCoins < coinsGoal && _gm.enemyDeathCount < killGoal)
             {
-                //not enough of both (both NPC)
+                //not enough of both (both NPC) "We want to see this and this"
+                _dialogObject.SetActive(true);
+                _dialogNPC.text = "We'd like to see a few more coins and enemies killed";
                 Debug.Log("Missing both");
             }
-            else if (_gm.collectedCoins <= coinsGoal)
+            else if (_gm.collectedCoins < coinsGoal)
             {
-                //not enough gold dialog (NPC 1)
+                //not enough gold dialog (NPC 1) "I still want to see this many coins, this many left to get"
+                _dialogObject.SetActive(true);
+                _dialogNPC.text = "I'd like to see a few more coins collected!";
                 Debug.Log("Missing coins");
             }
-            else if (_gm.enemiesDefeated <= killGoal)
+            else if (_gm.enemyDeathCount < killGoal)
             {
-                //nog enough kills (NPC 2)
-                Debug.Log("Missing kills");
+                //nog enough kills (NPC 2) "I still want to see this many kills, this many left to get"
+                _dialogObject.SetActive(true);
+                _dialogNPC.text = "Could you defeat a few more enemies for me?";
+                Debug.Log(_gm.enemyDeathCount);
             }
-
+            else
+            {
+                Debug.Log("Requirements met!");
+            }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _dialogObject.SetActive(false);
+        Debug.Log("Deactivate dialog");
     }
 }
 
